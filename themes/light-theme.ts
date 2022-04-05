79b39a4734a631307d75eba53047f231964e1e58
Leo Omaña <leotangram@gmail.com>
2022-04-05 15:19:22 -0500

    Implement sidebar

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -16,5 +16,13 @@ export const lightTheme = createTheme({
     error: {
       main: red.A400
     }
+  },
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        elevation: 0
+      },
+      styleOverrides: {}
+    }
   }
 })
