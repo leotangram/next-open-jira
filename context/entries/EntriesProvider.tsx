d1fdebeca2ca55a91233fdefdf34ebe2c8ffe9c8
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-05-17 17:04:59 -0500

    Next API

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,8 +1,8 @@
-import { FC, useReducer } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import { FC, useEffect, useReducer } from 'react'
 
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from '../../interfaces'
+import { entriesApi } from '../../apis'
 
 export interface EntriesState {
   entries: Entry[]
@@ -15,19 +15,37 @@ const Entries_INITIAL_STATE: EntriesState = {
 export const EntriesProvider: FC = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      createdAt: Date.now(),
-      status: 'pending'
-    }
+  useEffect(() => {
+    refreshEntries()
+  }, [])
 
-    dispatch({ type: '[Entry] - Add-Entry', payload: newEntry })
+  const refreshEntries = async () => {
+    const { data } = await entriesApi.get<Entry[]>('/entries')
+    dispatch({ type: '[Entry] - Refresh-Data', payload: data })
   }
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: '[Entry] - Entry-Updated', payload: entry })
+  const addNewEntry = async (description: string) => {
+    try {
+      const { data } = await entriesApi.post<Entry>('/entries', {
+        description
+      })
+
+      dispatch({ type: '[Entry] - Add-Entry', payload: data })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status
+      })
+      dispatch({ type: '[Entry] - Entry-Updated', payload: data })
+    } catch (error) {
+      console.log({ error })
+    }
   }
 
   return (
--- models/Entry.ts
+++ models/Entry.ts
@@ -1,7 +1,7 @@
 import mongoose, { Model, Schema } from 'mongoose'
 import { Entry } from '../interfaces'
 
-interface IEntry extends Entry {}
+export interface IEntry extends Entry {}
 
 const entrySchema = new Schema({
   description: { type: String, required: true },
@@ -11,7 +11,8 @@ const entrySchema = new Schema({
     enum: {
       values: ['pending', 'in-progress', 'finished'],
       message: '{VALUE} no es un estado permitido'
-    }
+    },
+    default: 'pending'
   }
 })
 
